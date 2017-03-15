787523c4a7ac3a3763e4045a3bc5eb0a9fdaed3f
Piotr Leszkowicz <leleleszek@gmail.com>
2017-03-15 11:25:14 +0400

    added methods

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
 'use strict';
 
 let request = require('request')
+// const url = 'http://52.90.90.73/'
+const url = 'http://weathered-rain-1953.getsandbox.com/move'
+
+
+function move(x, y) {
+  request(`${url}/move/${x},${y}`, function (error, response, body) {
+    console.log(body)
+  })
+}
+
+function shoot(x, y) {
+  request(`${url}/shoot/${x},${y}`, function (error, response, body) {
+    console.log(body)
+  })
+
+}
+
+function shield(x, y) {
+  request(`${url}/shield/${x},${y}`, function (error, response, body) {
+    console.log(response)
+  })
+}
+
+function shield2(x1, y1, x2, y2 ) {
+  request(`${url}/shield/${x1},${y1};${x2},${y2}`, function (error, response, body) {})
+}
 
 setInterval(() => {
-  // request('http://52.90.90.73/8h35nwk8/shoot/0,1', function (error, response, body) {
-  request('https://jsonplaceholder.typicode.com/posts/1', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-  })
+  move(1,2)
+  shoot(1,2)
 }, 1000)
